247d0f49d2481c173714ada240ea645c9c919c55
Albert <albert.darchiev.7e6@itb.cat>
2023-03-27 21:05:32 +0200

    splashScreenLottie

--- app/src/main/java/com/example/m08_mapsapp/view/SplashFragment.kt
+++ app/src/main/java/com/example/m08_mapsapp/view/SplashFragment.kt
@@ -1,30 +1,26 @@
 package com.example.m08_mapsapp.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.m08_mapsapp.R
 
 class SplashFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        val view =  inflater.inflate(R.layout.fragment_splash, container, false)
+        Handler(Looper.myLooper()!!).postDelayed({
+findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        },3100)
+        return view
     }
 
 }
 
 
-//<com.airbnb.lottie.LottieAnimationView
-//android:id="@+id/animationView"
-//android:layout_width="match_parent"
-//android:layout_height="wrap_content"
-//app:lottie_url="https://assets3.lottiefiles.com/packages/lf20_1ezvulr6.json"
-//app:lottie_autoPlay="true"
-//app:lottie_loop="true"
-//app:layout_constraintStart_toStartOf="parent"
-//app:layout_constraintEnd_toEndOf="parent"
-//app:layout_constraintBottom_toBottomOf="parent"
-//app:layout_constraintTop_toTopOf="parent"/>
